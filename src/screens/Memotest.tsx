2e71f16ebeb826eb59308acc28afb811918d0f11
Agustina Gomez Fernandez <agustinagomez385@gmail.com>
2023-02-23 14:43:20 -0300

    finished memotest

--- src/screens/Memotest.tsx
+++ src/screens/Memotest.tsx
@@ -1,5 +1,13 @@
-import { ImageList, ImageListItem } from "@mui/material";
+import {
+	ImageList,
+	ImageListItem,
+	Box,
+	Collapse,
+	Alert,
+	IconButton,
+} from "@mui/material";
 import { useEffect, useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
 
 const IMAGES = [
 	"https://icongr.am/devicon/angularjs-original.svg?size=128&color=currentColor",
@@ -18,48 +26,83 @@ const IMAGES = [
 
 export default function Memotest() {
 	const [guessed, setGuesssed] = useState<string[]>([]);
-	const [select, setSelected] = useState<string[]>([]);
+	const [selected, setSelected] = useState<string[]>([]);
+	const [open, setOpen] = useState(false);
 
 	useEffect(() => {
-		if (select.length == 2) {
-			if (select[0] === select[1]) {
-				setGuesssed((guessed) => guessed.concat(select));
+		if (selected.length == 2) {
+			if (selected[0].split("|")[1] === selected[1].split("|")[1]) {
+				setGuesssed((guessed) => guessed.concat(selected));
 			}
 			setTimeout(() => setSelected([]), 1000);
 		}
-	}, [select]);
+	}, [selected]);
+
+	useEffect(() => {
+		if (guessed.length === IMAGES.length) {
+			// alert("You Win!");
+			setOpen(true);
+			location.reload();
+		}
+	}, [guessed]);
 
 	return (
-		<ImageList sx={{ width: 900, maxHeight: 900 }} cols={5} gap={20}>
-			{IMAGES.map((image) => {
-				const [, url] = image.split("|");
-				return (
-					<ImageListItem
-						onClick={() =>
-							select.length < 2 && setSelected((select) => select.concat(image))
-						}
-						key={url}
-						style={{
-							cursor: "pointer",
-							padding: 12,
-							border: "1px solid #555",
-							borderRadius: 12,
-						}}
-					>
-						{select.includes(image) || guessed.includes(image) ? (
-							<img alt="icon" src={url} />
-						) : (
-							<img
-								key={url}
-								alt="icon"
-								src={
-									"https://icongr.am/clarity/search.svg?size=128&color=currentColor"
-								}
-							/>
-						)}
-					</ImageListItem>
-				);
-			})}
-		</ImageList>
+		<>
+			{open ? (
+				<Box sx={{ width: "100%" }}>
+					<Collapse in={open}>
+						<Alert
+							action={
+								<IconButton
+									aria-label="close"
+									color="inherit"
+									size="small"
+									onClick={() => {
+										setOpen(false);
+									}}
+								>
+									<CloseIcon fontSize="inherit" />
+								</IconButton>
+							}
+							sx={{ mb: 2 }}
+						>
+							You Win!!!
+						</Alert>
+					</Collapse>
+				</Box>
+			) : null}
+			<ImageList sx={{ width: "100%", maxHeight: "80%" }} cols={5} gap={20}>
+				{IMAGES.map((image) => {
+					const [, url] = image.split("|");
+					return (
+						<ImageListItem
+							onClick={() =>
+								selected.length < 2 &&
+								setSelected((selected) => selected.concat(image))
+							}
+							key={image}
+							style={{
+								cursor: "pointer",
+								padding: 12,
+								border: "1px solid #555",
+								borderRadius: 12,
+							}}
+						>
+							{selected.includes(image) || guessed.includes(image) ? (
+								<img alt="icon" src={url} />
+							) : (
+								<img
+									key={url}
+									alt="icon"
+									src={
+										"https://icongr.am/clarity/search.svg?size=128&color=currentColor"
+									}
+								/>
+							)}
+						</ImageListItem>
+					);
+				})}
+			</ImageList>
+		</>
 	);
 }
